b4b265bd03932d75193c4b583e7687188a9a94b7
Daniel Pinto <cayan@programmer.net>
2016-03-15 14:40:18 -0300

    Initial commit.

---
+++ app/main.ts
@@ -0,0 +1,13 @@
+import {bootstrap} from 'angular2/platform/browser';
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'app',
+    directives: [],
+    template: ``
+})
+class App {
+
+};
+
+bootstrap(App, []);
